a0b61174786fe9218fca9e44e8d82c65fc46a630
Rob van Son <r.j.j.h.vanson@gmail.com>
2016-03-24 16:23:14 +0100

    language support

---
+++ internationalization_tables.js
@@ -0,0 +1,72 @@
+var userLanguage = (navigator.language) ? navigator.language : navigator.userLanguage;
+userLanguage = userLanguage.substr(0,2).toUpperCase();
+
+function set_language (language) {
+	var labels = internationalization_tables[language];
+	for(x in labels) {
+		if(document.getElementById(x)) {
+			document.getElementById(x).textContent = labels[x][0];
+		};
+	};
+};
+var internationalization_tables = {
+	ZH: {
+		Record: ["录音", "录音。录音时间为4秒钟，注意“录音指示灯”。"],
+		Play: ["播放", "播放您的发音。"],
+		Example: ["参考发音", "播放软件提供的参考发音。"],
+		Previous: ["上一个", "切换到上一个单词。"],
+		Next: ["下一个", "切换到下一个单词。"],
+		WordlistCaption: ["单词表", "更换单词表。"],
+		SelectWords: ["选择单词", "选择要练习的单词"],
+		WordlistUp: ["上一个", "切换到上一个单词表。"],
+		WordlistDown: ["下一个", "切换到下一个单词表。"]
+		},
+	EN: {
+		Record: ["Record", "Record your pronunciation. You have 4 seconds, watch the recording 'light'"],
+		Play: ["Play", "Play back of your recorded pronunciation"],
+		Example: ["Example", "Play example tone pronunciation"],
+		Previous: ["Previous", "Previous word"],
+		Next: ["Next", "Next word"],
+		WordlistCaption: ["Word List", "Change word list"],
+		SelectWords: ["Words", "Select words to practise"],
+		WordlistUp: ["Previous", "Previous word list"],
+		WordlistDown: ["Next", "Next word list"]
+		},
+	JA: {
+		Record: ["録音", "声を録音します。録音されるのは4秒間です。録音中“録音ライト”が表示されます。"],
+		Play: ["再生", "録音した声を再生します。"],
+		Example: ["発音例", "発音例を再生します。"],
+		Previous: ["戻る", "一つ前の単語に戻ります。"],
+		Next: ["次へ", "次の単語に進みます。"],
+		WordlistCaption: ["単語表", "単語表を変更します。"],
+		SelectWords: ["単語の選択", "練習で使う単語を選択します"],
+		WordlistUp: ["戻る", "一つ前の単語表に戻ります。"],
+		WordlistDown: ["次へ", "次の単語表に進みます。"]
+		},
+	DE: {
+		Record: ["Aufnahme", "Machen Sie eine Sprachaufnahme. Sie haben 4 Sekunden bevor die Aufnahme beginnt; achten Sie auf das rote Licht."],
+		Play: ["Wiedergabe", "Abspielen der Aufnahme"],
+		Example: ["Vorbild", "Vorbild abspielen"],
+		Previous: ["Zurück", "zum vorigen Abschnitt"],
+		Next: ["Voraus", "zum nächsten Abschnitt"],
+		WordlistCaption: ["Wordliste", "Ändere Wordliste"],
+		SelectWords: ["Wörter", "Wähle die Wörter zum üben"],
+		WordlistUp: ["vorige", "Zur voriger Liste"],
+		WordlistDown: ["nächste", "Zur nächster Liste"]
+		},
+	NL: {
+		Record: ["Opnemen", ""],
+		Play: ["Afspelen", ""],
+		Example: ["Voorbeeld", ""],
+		Previous: ["Terug", ""],
+		Next: ["Vooruit", "Kies de woorden om te oefenen"],
+		WordlistCaption: ["Woordenlijst", "Andere woordenlijst"],
+		SelectWords: ["Woorden", ""],
+		WordlistUp: ["Vorige", "Vorige woordenlijst"],
+		WordlistDown: ["Volgende", "Volgende woordenlijst"]
+	}
+};
+
+if(!internationalization_tables[userLanguage]) {
+	userLanguage = undefined;
+};
