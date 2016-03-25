d131f5e8773ee9036e687379b8e66c5d8ce015c3
Rob van Son <r.j.j.h.vanson@gmail.com>
2016-03-25 11:41:56 +0100

    Added local wordlists

---
+++ wordlists_plus.js
@@ -0,0 +1,14 @@
+/*
+ *	Wordlists added here will be automatically added to the global list 
+ */
+ 
+var wordlists_plus = [
+// Add your wordlists here
+	[
+		"Test", 
+		[["ni3hao3","nǐhǎo","你好","hello!","-","-"],["qing3wen4","qǐngwèn","请问","May I ask..?","-","-"]]
+	] 
+]
+
+
+wordlists.push.apply(wordlists, wordlists_plus);
