5ef6ea35c03d2aaa386ba654ccd19d93eaaadac6
Rob van Son <R.J.J.H.vanSon@gmail.com>
2016-04-02 17:17:12 +0200

    Added language choice. Incomplete (reduplicates selector options)

--- internationalization_tables.js
+++ internationalization_tables.js
@@ -28,7 +28,7 @@ function set_language (language) {
 		};
 	};
 	
-	labels = register_tables[language];
+	labels = selector_tables[language];
 	for(x in labels) {
 		if(document.getElementById(x)) {
 			document.getElementById(x).title = labels[x][1];
@@ -46,6 +46,33 @@ function set_language (language) {
 			};
 		};
 	};
+	
+	labels = language_table;
+	for(x in language_table) {
+		if(document.getElementById(x)) {
+			document.getElementById(x).title = labels[x][1];
+			var defaultText = document.getElementById(x+"Caption");
+			defaultText.textContent = labels[x][0]
+		} else if (x.match(/_/)) {
+			var Id = x.replace(/_[^_]*$/, "");
+			var value = x.replace(/^[^_]*_/, "");
+			if(document.getElementById(Id)) {
+				var selector = document.getElementById(Id);
+				var newOption = selector.options[0].cloneNode(true);
+				newOption.value = value;
+				newOption.text = labels[x][0];
+				selector.add(newOption);
+			};
+		};
+	};
+};
+
+function change_language () {
+	var index = document.getElementById("Language").selectedIndex;
+	var value = document.getElementById("Language").options[index].value;
+	userLanguage = value;
+	set_language (userLanguage);
+	return userLanguage;
 };
 
 var internationalization_tables = {
@@ -113,18 +140,20 @@ function getRegister () {
 	return value;
 }
 
-var register_tables = {
+var selector_tables = {
 	ZH: {
-		Register: ["Your Voice", "Select the category that fits your voice"],
-		Register_350: ["Woman High", "Pick this if you are a female with a high voice"],
-		Register_300: ["Woman Mid", "Pick this if you are a female with a mid-range voice"],
-		Register_250: ["Woman Low", "Pick this if you are a female with a low voice"],
-		Register_249: ["Man High", "Pick this if you are a male with a high voice"],
-		Register_180: ["Man Mid", "Pick this if you are a male with a mid-range voice"],
-		Register_150: ["Man Low", "Pick this if you are a male with a low voice"],
-		Register_125: ["Man Xtr Low", "Pick this if you are a male with a very low voice"]
+		Language: ["语言", "选择页面的显示语言。"],
+		Register:	["您的声音", "设置您的声音类型。"],
+		Register_350:	["女性 高 W", "如果发音人为女性且声音较高，那么选择此项。"],
+		Register_300:	["女性 中 i", "如果发音人为女性且声音高度适中，那么选择此项。"],
+		Register_250:	["女性 低 o", "如果发音人为女性且声音较低，那么选择此项。"],
+		Register_249:	["男性 高 M", "如果发音人为男性且声音较高，那么选择此项。"],
+		Register_180:	["男性 中 a", "如果发音人为男性且声音高度适中，那么选择此项。"],
+		Register_150:	["男性 低 L", "如果发音人为男性且声音较低，那么选择此项。"],
+		Register_125:	["男性 超低", "如果发音人为男性且声音很低，那么选择此项。"]
 		},
 	EN: {
+		Language: ["Language", "Select a language"],
 		Register: ["Your Voice", "Select the category that fits your voice"],
 		Register_350: ["Woman High", "Pick this if you are a female with a high voice"],
 		Register_300: ["Woman Mid", "Pick this if you are a female with a mid-range voice"],
@@ -135,35 +164,46 @@ var register_tables = {
 		Register_125: ["Man Xtr Low", "Pick this if you are a male with a very low voice"]
 		},
 	JA: {
-		Register: ["Your Voice", "Select the category that fits your voice"],
-		Register_350: ["Woman High", "Pick this if you are a female with a high voice"],
-		Register_300: ["Woman Mid", "Pick this if you are a female with a mid-range voice"],
-		Register_250: ["Woman Low", "Pick this if you are a female with a low voice"],
-		Register_249: ["Man High", "Pick this if you are a male with a high voice"],
-		Register_180: ["Man Mid", "Pick this if you are a male with a mid-range voice"],
-		Register_150: ["Man Low", "Pick this if you are a male with a low voice"],
-		Register_125: ["Man Xtr Low", "Pick this if you are a male with a very low voice"]
+		Language: ["言語", "表示言語を設定します。"],
+		Register:	["発話者の声", "発話者の声のカテゴリーを設定します。"],
+		Register_350:	["女性　高め W", "発話者が女性でかつ声が高めのとき選択します。"],
+		Register_300:	["女性　中間 i", "発話者が女性でかつ声が中間の高さのとき選択します。"],
+		Register_250:	["女性　低め o", "発話者が女性でかつ声が低めのとき選択します。"],
+		Register_249:	["男性　高め M", "発話者が男性でかつ声が高めのとき選択します。"],
+		Register_180:	["男性　中間 a", "発話者が男性でかつ声が中間の高さのとき選択します。"],
+		Register_150:	["男性　低め L", "発話者が男性でかつ声が低めのとき選択します。"],
+		Register_125:	["男性　更に低め", "発話者が男性でかつ声が更に低めのとき選択します。"]
 		},
 	DE: {
-		Register: ["Your Voice", "Select the category that fits your voice"],
-		Register_350: ["Woman High", "Pick this if you are a female with a high voice"],
-		Register_300: ["Woman Mid", "Pick this if you are a female with a mid-range voice"],
-		Register_250: ["Woman Low", "Pick this if you are a female with a low voice"],
-		Register_249: ["Man High", "Pick this if you are a male with a high voice"],
-		Register_180: ["Man Mid", "Pick this if you are a male with a mid-range voice"],
-		Register_150: ["Man Low", "Pick this if you are a male with a low voice"],
-		Register_125: ["Man Xtr Low", "Pick this if you are a male with a very low voice"]
+		Language: ["Sprache", "Wähle die gewünschte Sprache"],
+		Register:	["Ihre Stimme", "Wähle die Category die past bei Ihrer Stimme"],
+		Register_350:	["Frau hoch", "Wähle diesen Knopf wenn Sie eine Frau mit eine hohe Stimme sind"],
+		Register_300:	["Frau mittel", "Wähle diesen Knopf wenn Sie eine Frau mit eine mittel-hohe Stimme sind"],
+		Register_250:	["Frau tief", "Wähle diesen Knopf wenn Sie eine Frau mit eine tiefe Stimme sind"],
+		Register_249:	["Man hoch", "Wähle diesen Knopf wenn Sie ein Mann mit eine hohe Stimme sind"],
+		Register_180:	["Man mittel", "Wähle diesen Knopf wenn Sie ein Mann mit eine mittle-hohe Stimme sind"],
+		Register_150:	["Man tief", "Wähle diesen Knopf wenn Sie ein Mann mit eine tiefe Stimme sind"],
+		Register_125:	["Man Xtr tief", "Wähle diesen Knopf  wenn Sie ein Mann mit eine sehr tiefe Stimme sind"]
 		},
 	NL: {
-		Register: ["Your Voice", "Select the category that fits your voice"],
-		Register_350: ["Woman High", "Pick this if you are a female with a high voice"],
-		Register_300: ["Woman Mid", "Pick this if you are a female with a mid-range voice"],
-		Register_250: ["Woman Low", "Pick this if you are a female with a low voice"],
-		Register_249: ["Man High", "Pick this if you are a male with a high voice"],
-		Register_180: ["Man Mid", "Pick this if you are a male with a mid-range voice"],
-		Register_150: ["Man Low", "Pick this if you are a male with a low voice"],
-		Register_125: ["Man Xtr Low", "Pick this if you are a male with a very low voice"]
+		Language: ["Taal", "Selecteer de gewenste taal"],
+		Register:	["Uw Stem", "Kies de categorie die past bij uw stem"],
+		Register_350:	["Vrouw Hoog", "Kies dit als u een vrouw bent met een hoge stem"],
+		Register_300:	["Vrouw Mid", "Kies dit als u een vrouw bent met een middel-hoge stem"],
+		Register_250:	["Vrouw Laag", "Kies dit als u een vrouw bent met een lage stem"],
+		Register_249:	["Man Hoog", "Kies dit als u een man bent met een hoge stem"],
+		Register_180:	["Man Mid", "Kies dit als u een man bent met een middel-lage stem"],
+		Register_150:	["Man Laag", "Kies dit als u een man bent met een lage stem"],
+		Register_125:	["Man Xtr Laag", "Kies dit als u een man bent met een zeer lage stem"]
 		},
+}
+
+var language_table = {
+	Language_ZH: ["汉语", "汉语版"],
+	Language_EN: ["English", "English language version"],
+	Language_JA: ["日本語", "日本語版"],
+	Language_DE: ["Deutsch", "Deutsche Version"],	
+	Language_NL: ["Nederlands", "Gebruik Nederlands"]	
 }
 
 if(!internationalization_tables[userLanguage]) {
