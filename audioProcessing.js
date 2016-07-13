9f4450ac0d460ca6c6afe4ea7348590ec3cefdd1
Rob van Son <R.J.J.H.vanSon@gmail.com>
2016-07-13 15:24:18 +0200

    Merged audioProcessing.js

--- audioProcessing.js
+++ audioProcessing.js
@@ -19,10 +19,12 @@
 
 // Global variables
 var recordedBlob, recordedBlobURL;
-var recordedArray, currentAudioWindow;
-var recordedSampleRate, recordedDuration;
+var recordedArray;
+var recordedDuration;
 var recordedPitchTier;
 var windowStart = windowEnd = 0;
+var recordedSampleRate = 0;
+var currentAudioWindow = undefined;
 
 var clearRecording = function () { 
 	recordedBlob = undefined;
@@ -96,6 +98,60 @@ function play_soundArray (soundArray, sampleRate, start, end) {
 	source.connect(audioContext.destination);
 	// start the source playing
 	source.start();
+};
+
+/*
+ * Convert typed mono array to WAV blob
+ * 
+ */
+function writeUTFBytes(view, offset, string){ 
+  var lng = string.length;
+  for (var i = 0; i < lng; i++){
+    view.setUint8(offset + i, string.charCodeAt(i));
+  }
+}
+
+// Write selection [start, end] to a WAV blob
+function arrayToBlob (array, start, end, sampleRate) {
+	if(!array) return;
+	var window = array.slice(start*sampleRate, end*sampleRate);
+
+	// create the buffer and view to create the .WAV file
+	var buffer = new ArrayBuffer(44 + window.length * 2);
+	var view = new DataView(buffer);
+
+	// write the WAV container, check spec at: https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+	// RIFF chunk descriptor
+	writeUTFBytes(view, 0, 'RIFF');
+	view.setUint32(4, 44 + window.length * 2, true);
+	writeUTFBytes(view, 8, 'WAVE');
+	// FMT sub-chunk
+	writeUTFBytes(view, 12, 'fmt ');
+	view.setUint32(16, 16, true);
+	view.setUint16(20, 1, true);
+	// mono (2 channels)
+	view.setUint16(22, 1, true);
+	view.setUint32(24, sampleRate, true);
+	view.setUint32(28, sampleRate * 2, true);
+	view.setUint16(32, 2, true);
+	view.setUint16(34, 16, true);
+	// data sub-chunk
+	writeUTFBytes(view, 36, 'data');
+	view.setUint32(40, window.length * 2, true);
+
+	// write the PCM samples
+	var lng = window.length;
+	var index = 44;
+	var volume = 1;
+	for (var i = 0; i < lng; i++){
+	    view.setInt16(index, window[i] * (0x7FFF * volume), true);
+	    index += 2;
+	}
+	
+	// our final binary blob that we can hand off
+	var blob = new Blob ( [ view ], { type : 'audio/wav' } );
+	
+	return blob;
 };
 
 // Set up window 
