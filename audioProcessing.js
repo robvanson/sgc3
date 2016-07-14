d1485ef160f2049f619cc5d262e0a9125de10d85
Rob van Son <r.j.j.h.vanson@gmail.com>
2016-07-14 15:48:55 +0200

    Save Audio to IndexedDB

--- audioProcessing.js
+++ audioProcessing.js
@@ -483,3 +483,96 @@ function get_time_of_minmax (points) {
 	};
 	return {min: min, max: max, tmin: tmin, tmax: tmax};
 };
+
+// Use IndexedDB as an Audio storage
+// Remove entries that have the same name
+// The structure is: Directory, Filename, Binary data
+function addAudioBlob(storeName, name, blob) {
+	var date = new Date().toLocaleString();
+	var db;
+	var request = indexedDB.open("Audio", 1);
+	request.onerror = function(event) {
+	  alert("Use of IndexedDB not allowed");
+	};
+	request.onsuccess = function(event) {
+		db = this.result;
+		var request = db.transaction([storeName], "readwrite")
+			.objectStore(storeName)
+			.put({ name: name, date: date, audio: blob });
+		
+		request.onsuccess = function(event) {
+			console.log("Success: ", this.result, " ", date);
+
+		};
+		
+		// If data already exist, update it
+		request.onerror = function(event) {
+			alert("Unable to add data\r\n"+name+" cannot be created or updated");
+			console.log("Unable to add data: "+name+" cannot be created or updated");
+		};
+	};
+
+	request.onupgradeneeded = function(event) {
+		var db = this.result;
+		// Create an objectStore to hold audio blobs.
+		var objectStore = db.createObjectStore(storeName, { keyPath: "name", autoincrement: false });
+		// Use transaction oncomplete to make sure the objectStore creation is 
+		// finished before adding data into it.
+		objectStore.transaction.oncomplete = function(event) {
+			// Store values in the newly created objectStore.
+			var date = new Date().toLocaleString();
+			var customerObjectStore = db.transaction([storeName], "readwrite").objectStore(storeName);
+			customerObjectStore.add({name: name, date: date, audio: blob });
+			request.onsuccess = function(event) {
+				console.log("Success: ", this.result, " ", date);
+	
+			};
+			
+			request.onerror = function(event) {
+				alert("Unable to add data\r\n"+name+" cannot be created or updated");
+				console.log("Unable to add data: "+name+" cannot be created or updated");
+			};
+		};
+	};
+};
+
+// Remove Audio storage, including ALL data
+function clearDataStorage (databaseName, storeName) {
+	var db;
+	var request = indexedDB.open(databaseName, 1);
+	request.onerror = function(event) {
+	  alert("Use of IndexedDB not allowed");
+	};
+	request.onsuccess = function(event) {
+		db = this.result;
+		var request = db.transaction([storeName], "readwrite")
+			.objectStore(storeName)
+			.clear();
+		
+		request.onsuccess = function(event) {
+			console.log("Success: ", storeName);
+
+		};
+		
+		// If data already exist, update it
+		request.onerror = function(event) {
+			alert("Unable to clear "+storeName);
+			console.log("Unable to clear "+storeName);
+		};
+	};
+};
+	
+
+// Remove Audio storage, including ALL data
+function deleteDatabase (databaseName) {
+	var req = indexedDB.deleteDatabase(databaseName);
+	req.onsuccess = function () {
+	    console.log("Deleted database successfully");
+	};
+	req.onerror = function () {
+	    console.log("Couldn't delete database");
+	};
+	req.onblocked = function () {
+	    console.log("Couldn't delete database due to the operation being blocked");
+	};
+};
